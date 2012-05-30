9bf9cc0fa840b755eafddb2070b3b4faa6eebc39
silence <jquan.zhu@gmail.com>
2012-05-30 10:06:16 +0800

    modified main.c

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Dump(char *log)
+typedef int BOOL;
+typedef int bool;
+
+#define TRUE 1
+#define true 1
+#define FALSE 0
+#define false 0
+
+#define DEBUG true
+
+static void Dump(char *log)
 {
-    printf("-- DEBUG: %s --\r\n", log);
+    #if DEBUG == true
+	printf("-- DEBUG: %s -- \r\n", log);
+    #else
+	printf("-- INFO: %s -- \r\n", log);
+    #endif
 }
 
 int main(int argc, char* argv[])
